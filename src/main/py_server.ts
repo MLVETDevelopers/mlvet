ef7e4e473807d6d254c3f3bf01423d270d2cbd76
Taz17 <mutaz.ashhab@gmail.com>
2022-03-21 15:33:13 +1100

    Seperate `src/py_server` in path.join into individual arguments to avoid errors on other OS's

--- src/main/py_server.ts
+++ src/main/py_server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default function startServer() {
   const oldWorkingDirectory = process.cwd();
-  const pathToServer = path.join(process.cwd(), '/src/py_server');
+  const pathToServer = path.join(process.cwd(), 'src', 'py_server');
 
   process.chdir(pathToServer);
   let proc = null;

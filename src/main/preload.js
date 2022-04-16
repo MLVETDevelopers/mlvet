72b5f8caba1819ce5d0e16810ff31cc1def4ed7b
Taz17 <mutaz.ashhab@gmail.com>
2022-04-16 13:12:48 +1000

    Update preload.js to expose function rather than ipcRenderer

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,6 +1,11 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+/*
+ * When exposing a new method make sure to update global.d.ts
+ * (src/renderer/global.d.ts) with the method signature with types
+ * to help out typescipt
+ */
+
 contextBridge.exposeInMainWorld('electron', {
-  // TODO: dont expose whole renderer for security reasons
-  ipcRenderer,
+  requestMediaDialog: () => ipcRenderer.invoke('import-media'),
 });

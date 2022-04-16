76842fab82adcce9e6bd73bcde4266add1a134b4
Taz17 <mutaz.ashhab@gmail.com>
2022-04-16 13:11:54 +1000

    delete `ipc.ts`

--- src/renderer/ipc.ts
+++
@@ -1,6 +0,0 @@
-const { ipcRenderer } = window.electron;
-
-const requestMediaDialog: () => Promise<string | null> = () =>
-  ipcRenderer.invoke('import-media');
-
-export default requestMediaDialog;

a74515c9421ff8f02f639c56fe1e04b76335f026
Taz17 <mutaz.ashhab@gmail.com>
2022-04-16 13:13:47 +1000

    Add function signature to `global.d.ts` and remove ipcRenderer

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -1,12 +1,10 @@
 // Let TypeScript know that the ipcRenderer is on the window object.
 // If you need to use other modules from electron in the renderer, add their types here and then reference from window.electron
 
-import { IpcRenderer } from 'electron';
-
 declare global {
   interface Window {
     electron: {
-      ipcRenderer: IpcRenderer;
+      requestMediaDialog: () => Promise<string | null>;
     };
   }
 }

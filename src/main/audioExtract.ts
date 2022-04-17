6fbe8571361e6e3c889ac56cb712384e927c3e80
Taz17 <mutaz.ashhab@gmail.com>
2022-04-17 20:17:45 +1000

    Use `cwd` option childprocess.spawn

--- src/main/audioExtract.ts
+++ src/main/audioExtract.ts
@@ -4,30 +4,27 @@ import path from 'path';
 export default function extractAudio(
   absolutePathToMediaFile: string
 ): ChildProcess {
-  const pathToSaveMedia = path.join(
-    process.cwd(),
-    'assets',
-    'videos',
-    'audio.wav'
-  );
+  const pathToSaveMedia = path.join(process.cwd(), 'assets', 'videos');
 
   // Original command is:
   // ffmpeg -i input.mp4 -ac 1 -ar 16000 -vn output.wav
   // i've added `-f wav` which forces the output to .wav
   // TODO: figure out how to save output to a directory as right now it saves the output to the directory it is in
   // docs: https://ffmpeg.org/ffmpeg.html
-  const proc = spawn('ffmpeg', [
-    '-i',
-    absolutePathToMediaFile,
-    '-ac',
-    '1',
-    '-ar',
-    '16000',
-    '-vn',
-    '-f',
-    'wav',
-    'audio.wav',
-  ]);
+  const proc = spawn(
+    'ffmpeg',
+    [
+      '-i',
+      absolutePathToMediaFile,
+      '-ac',
+      '1',
+      '-ar',
+      '16000',
+      '-vn',
+      'audio.wav',
+    ],
+    { cwd: pathToSaveMedia }
+  );
 
   return proc;
 }

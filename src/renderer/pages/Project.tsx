471445ef8f23454465542b9609d97c95b89035ff
Hugh Henry <hughcthenry@gmail.com>
2022-04-26 14:21:20 +1000

    Add initial transcription page layout

--- src/renderer/pages/Project.tsx
+++ src/renderer/pages/Project.tsx
@@ -1,5 +1,7 @@
-import { Button } from '@mui/material';
+import { Stack } from '@mui/material';
+import { Box } from '@mui/system';
 import { useDispatch, useSelector } from 'react-redux';
+import StandardButton from 'renderer/components/StandardButton';
 import { projectOpened } from 'renderer/store/actions';
 import { ApplicationStore } from '../store/helpers';
 
@@ -24,19 +26,54 @@ const ProjectPage = () => {
   };
 
   const saveButton = (
-    <Button onClick={() => window.electron.saveProject(currentProject)}>
+    <StandardButton onClick={() => window.electron.saveProject(currentProject)}>
       Save
-    </Button>
+    </StandardButton>
   );
 
-  const openButton = <Button onClick={handleOpenProject}>Open</Button>;
+  const openButton = (
+    <StandardButton onClick={handleOpenProject}>Open</StandardButton>
+  );
 
   return (
-    <div>
-      <div>{saveButton}</div>
-      <div>{openButton}</div>
-      Current project data: <pre>{JSON.stringify(currentProject)}</pre>
-    </div>
+    <>
+      <Stack
+        direction="row"
+        sx={{
+          height: 'calc(100% - 76px)',
+          gap: '48px',
+          px: '48px',
+          py: '32px',
+        }}
+      >
+        <Stack spacing={4} sx={{ flex: '5 1 0' }}>
+          <Box
+            sx={{
+              p: 2,
+              backgroundColor: '#515151',
+              overflow: 'auto',
+              height: '100%',
+            }}
+          >
+            {'transcription area '.repeat(150)}
+          </Box>
+        </Stack>
+        <Box sx={{ width: '2px', backgroundColor: 'gray' }} />
+        <Stack justifyContent="center" sx={{ width: 'fit-content' }}>
+          <Box sx={{ width: '400px', height: '280px', backgroundColor: 'red' }}>
+            video
+          </Box>
+          <div>
+            <div>{saveButton}</div>
+            <div>{openButton}</div>
+            {'Current project data: '}
+            <pre style={{ width: '200px', overflow: 'auto' }}>
+              {JSON.stringify(currentProject)}
+            </pre>
+          </div>
+        </Stack>
+      </Stack>
+    </>
   );
 };
 

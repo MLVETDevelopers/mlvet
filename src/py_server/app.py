db273e73f752c67199c3fb945be1b18c03dfd553
Kate Hyde <khyd0001@student.monash.edu>
2022-04-28 19:59:59 +1000

    Added GET endpoint for transctiption
    Added logic for the GET endpoint including raising an error if the filepath for the audio file isn't specified

--- src/py_server/app.py
+++ src/py_server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from background_removal import remove_background
+from flask import Flask, request, abort, jsonify
 from transcription import transcribe
 
 app = Flask(__name__)
+# app.config["DEBUG"] = True
 
 # READ: https://flask.palletsprojects.com/en/2.0.x/api/#flask.Request
 # to find out how to get incoming request data
@@ -12,14 +12,17 @@ app = Flask(__name__)
 def start():
     return "<p>Hello world</p>"
 
+@app.errorhandler(400)
+def bad_request_missing_audio_filepath(e):
+  return jsonify(error=str(e)), 400
 
-@app.route("/remove_bg", methods=["POST"])
-def handle_background():
-    remove_background()
-    return ""
-
-
-@app.route("/transcribe", methods=["POST"])
+@app.route("/transcribe", methods=["GET"])
 def handle_transcription():
-    transcribe()
-    return ""
+    audio_filepath = request.args.get("audiofilepath")
+    if audio_filepath is not None:
+      # return transcribe(audio_filepath)
+      return "{{'word': 'aword', 'start_time': 12.4, 'duration': 2}, {'word': 'anotherword', 'start_time': 15, 'duration': 1}}"
+    else:
+      abort(400, description="File path for audio file not included in request")
+
+# app.run()

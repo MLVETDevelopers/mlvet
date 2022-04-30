19fa154a93eb0a104c5c039d2ac573d8150a3bcc
Zhaohui Wang <zwan0202@student.monash.edu>
2022-04-30 13:50:50 +1000

    Update 'Transcription' type in 'sharedTypes'

--- src/sharedTypes.ts
+++ src/sharedTypes.ts
@@ -1,9 +1,16 @@
 // TODO: Define transcription schema (this is just a mockup)
-export type Transcription = string;
+export type Transcription = { words: [Word] };
+
+export type Word = {
+  word: string;
+  start_time: number;
+  duration: number;
+};
 
 export interface ProjectBase {
   schemaVersion: number;
   name: string;
+  savePath: string;
   filePath: string;
   transcription: Transcription | null;
 }

5d43148d95fd0d2e57a46ea71380769e9ea1a1be
Hugh Henry <hughcthenry@gmail.com>
2022-05-03 15:17:41 +1000

    Fix export card styling

--- src/renderer/components/ExportCard.tsx
+++ src/renderer/components/ExportCard.tsx
@@ -1,6 +1,15 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, styled, Typography } from '@mui/material';
 import ProgressBar from './ProgressBar';
 import colors from '../colors';
+
+const Card = styled(Stack)`
+  width: 232px;
+  height: 68px;
+  padding: 12px 16px;
+  gap: 6px;
+  background-color: ${colors.grey[600]};
+  border-radius: 5px;
+`;
 
 interface Props {
   progress: number;
@@ -8,22 +17,12 @@ interface Props {
 
 const ExportCard = ({ progress }: Props) => {
   return (
-    <Stack
-      sx={{
-        width: '232px',
-        height: '68px',
-        py: '12px',
-        px: '16px',
-        gap: '6px',
-        backgroundColor: colors.grey[600],
-        borderRadius: '5px',
-      }}
-    >
+    <Card>
       <Typography variant="h-300" color="primary.main">
         Exporting...
       </Typography>
       <ProgressBar variant="determinate" value={progress * 100} />
-    </Stack>
+    </Card>
   );
 };
 

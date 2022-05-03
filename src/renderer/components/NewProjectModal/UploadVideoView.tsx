7809f1ee7e7a7a32e13935e949427be9a7f39133
Taz17 <mutaz.ashhab@gmail.com>
2022-05-03 13:33:37 +1000

    Add base for `UploadVideoView` frame 99

---
+++ src/renderer/components/NewProjectModal/UploadVideoView.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  isOpen: boolean;
+  closeModal: () => void;
+  projectName: string;
+}
+const UploadVideoView = ({ isOpen, closeModal, projectName }: Props) => {
+  return 0;
+};
+
+export default UploadVideoView;

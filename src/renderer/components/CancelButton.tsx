f80dd00d529db4bfc8b5104a24ee5da77832e3dd
Taz17 <mutaz.ashhab@gmail.com>
2022-05-03 13:31:20 +1000

    Create `CancelButton` and `ActionButton` components

---
+++ src/renderer/components/CancelButton.tsx
@@ -0,0 +1,9 @@
+import { Button, styled } from '@mui/material';
+import colors from '../colors';
+
+const CancelButton = styled(Button)`
+  color: ${colors.yellow[500]};
+  background-color: ${colors.grey[600]};
+`;
+
+export default CancelButton;

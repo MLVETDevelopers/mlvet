9333239ecbdd9a00287d84afd63df150f10d4227
Taz17 <mutaz.ashhab@gmail.com>
2022-05-03 15:44:53 +1000

    Add ModalTitle component

---
+++ src/renderer/components/ModalTitle.tsx
@@ -0,0 +1,9 @@
+import { Box, colors, styled } from '@mui/material';
+
+const ModalTitle = styled(Box)`
+  color: ${colors.grey[500]};
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+export default ModalTitle;

1d4050dfbc289384afbaca2812fbe5ce8f7dbcc7
Patrick Brett <patrick@neara.com>
2022-05-04 16:11:31 +1000

    Clean up home page logic

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -1,16 +1,17 @@
 import { useSelector } from 'react-redux';
-import NewProjectBlock from 'renderer/components/NewProjectBlock';
-import RecentProjectsBlock from 'renderer/components/RecentProjectsBlock';
+import NewProjectBlock from '../components/NewProjectBlock';
+import RecentProjectsBlock from '../components/RecentProjectsBlock';
+import { ApplicationStore } from '../store/helpers';
 
 const HomePage = () => {
-  const recentProjects = useSelector(
-    (store: ApplicationStore) => store.recentProjects
+  const hasRecentProjects = useSelector(
+    (store: ApplicationStore) => store.recentProjects.length > 0
   );
 
   return (
     <>
-      <NewProjectBlock isFullSize={recentProjects.length === 0} />
-      {recentProjects.length > 0 && <RecentProjectsBlock />}
+      <NewProjectBlock isFullSize={!hasRecentProjects} />
+      {hasRecentProjects && <RecentProjectsBlock />}
     </>
   );
 };

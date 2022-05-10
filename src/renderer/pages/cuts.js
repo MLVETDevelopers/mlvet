a3ea4e0ac15000470249b176c1a4c2f35c5fb4fa
mgoy0001@student.monash.edu <mgoy0001@student.monash.edu>
2022-05-10 17:14:25 +1000

    add Video Preview - Pause, Play, Stop, Restart FE functionality

---
+++ src/renderer/pages/cuts.js
@@ -0,0 +1,16 @@
+const cuts = [
+  {
+    start: 459.2,
+    end: 459.5,
+  },
+  {
+    start: 459.2,
+    end: 459.5,
+  },
+  {
+    start: 459.2,
+    end: 459.5,
+  },
+];
+
+export default cuts;

072c02df67eddbd6b1c9d7729a9fc0541f5ac925
LiamTodd <toddliam26@gmail.com>
2022-05-11 14:47:26 +1000

    implement addSpaces

--- src/main/editDelete/preProcess.ts
+++ src/main/editDelete/preProcess.ts
@@ -18,25 +18,6 @@ const camelCase: MapCallback<SnakeCaseWord, PartialWord> = (word) => ({
 });
 
 /**
- * Adjusts durations so that words last until the next word, or until the end of the transcript
- * if the last word
- * @param totalDuration total duration of transcript
- * @returns word with durations updated
- */
-const fillDurationGaps: (
-  totalDuration: number
-) => MapCallback<PartialWord, PartialWord> =
-  (totalDuration) => (word, i, words) => {
-    const isLastWord = i === words.length - 1;
-    const durationUntil = isLastWord ? totalDuration : words[i + 1].startTime;
-
-    return {
-      ...word,
-      duration: roundToMs(durationUntil - word.startTime),
-    };
-  };
-
-/**
  * Injects extra attributes into a PartialWord to make it a full Word -
  * @param word the word to fill attributes for
  * @param i index of the word in the transcript
@@ -50,6 +31,72 @@ const injectAttributes: MapCallback<PartialWord, Word> = (word, i) => ({
   fileName: 'PLACEHOLDER FILENAME',
 });
 
+let TOTAL_DURATION = 0;
+/**
+ * Adds spaces between words which represent the silence between each word
+ * @param result The output being constructed
+ * @param word The current element of words
+ * @param index The index of word in words
+ * @param words The list of words being reduced
+ * @returns The updated transcript with a silence after word
+ */
+const addSpaces = (
+  result: Word[],
+  word: Word,
+  index: number,
+  words: Word[]
+) => {
+  const spaceChar = ' ';
+
+  // is the first word
+  if (index === 0) {
+    const preSilence: Word = {
+      word: spaceChar,
+      startTime: 0,
+      duration: roundToMs(words[index].startTime),
+      outputStartTime: 0,
+      deleted: false,
+      key: index.toString(),
+      fileName: 'PLACEHOLDER FILENAME',
+    };
+    result.push(preSilence);
+  }
+  // not the last word
+  if (index < words.length - 1) {
+    word.key = (index * 2 + 1).toString();
+    const silence: Word = {
+      word: spaceChar,
+      startTime: roundToMs(word.startTime + word.duration),
+      duration: roundToMs(
+        words[index + 1].startTime - word.startTime - word.duration
+      ),
+      outputStartTime: roundToMs(word.startTime + word.duration),
+      deleted: false,
+      key: (index * 2 + 2).toString(),
+      fileName: 'PLACEHOLDER FILENAME',
+    };
+    result.push(word);
+    result.push(silence);
+  }
+  // is last word
+  else {
+    word.key = (index * 2 + 1).toString();
+    const silence: Word = {
+      word: spaceChar,
+      startTime: roundToMs(word.startTime + word.duration),
+      duration: roundToMs(TOTAL_DURATION - word.startTime - word.duration),
+      outputStartTime: roundToMs(word.startTime + word.duration),
+      deleted: false,
+      key: (index * 2 + 2).toString(),
+      fileName: 'PLACEHOLDER FILENAME',
+    };
+    result.push(word);
+    result.push(silence);
+  }
+
+  return result;
+};
+
 /**
  * Pre processes a JSON transcript from python for use in the front end
  * @param jsonTranscript the JSON transcript input (technically a JS object but with some fields missing)
@@ -60,12 +107,13 @@ const preProcessTranscript = (
   jsonTranscript: JSONTranscription,
   duration: number
 ): Transcription => {
+  TOTAL_DURATION = duration;
   return {
     confidence: jsonTranscript.confidence,
     words: jsonTranscript.words
       .map(camelCase)
-      .map(fillDurationGaps(duration))
-      .map(injectAttributes),
+      .map(injectAttributes)
+      .reduce(addSpaces, []),
   };
 };
 

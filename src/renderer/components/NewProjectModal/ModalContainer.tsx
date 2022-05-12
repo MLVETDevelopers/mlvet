9ae54e6d61f635a68bffeed85cdfa8e116e671e4
exquisitebin <tcla0012@student.monash.edu>
2022-05-12 15:27:55 +1000

    Added some minor syntax upates

--- src/renderer/components/NewProjectModal/ModalContainer.tsx
+++ src/renderer/components/NewProjectModal/ModalContainer.tsx
@@ -16,10 +16,7 @@ const CustomModal = styled(Modal)`
 
 const CustomModalInner = styled(Box)`
   background: ${colors.grey[700]};
-  padding-left: 30px;
-  padding-right: 30px;
-  padding-bottom: 30px;
-  padding-top: 15px;
+  padding: 15px 30px 30px 30px;
   border-radius: 5px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;

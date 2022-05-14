83b325c9efe29d0504614b71cbe5be1683c1ac0d
Vita1287 <jzho0079@student.monash.edu>
2022-05-14 17:47:09 +1000

    Added liveProcess test for deletion

---
+++ src/main/editDelete/__tests__/liveProcess.test.ts
@@ -0,0 +1,110 @@
+import liveProcessTranscript from '../liveProcess';
+
+describe('Test live-processing Transcription outputStartTime after edit/delete', () => {
+  it('should produce expected Transcription after delete operation', () => {
+    const inputTranscript = {
+      confidence: 1,
+      words: [
+        {
+          word: ' ',
+          startTime: 0,
+          duration: 0.5,
+          outputStartTime: 0,
+          deleted: false,
+          key: '0',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'abc',
+          duration: 1,
+          startTime: 0.5,
+          outputStartTime: 0.5,
+          key: '1',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 1.5,
+          duration: 0.5,
+          outputStartTime: 1.5,
+          deleted: false,
+          key: '2',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'def',
+          duration: 1,
+          startTime: 2,
+          outputStartTime: 2,
+          key: '3',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 3,
+          duration: 0.5,
+          outputStartTime: 3,
+          deleted: false,
+          key: '4',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+      ],
+    };
+
+    inputTranscript.words[1].deleted = true;
+    const outputTranscript = liveProcessTranscript(inputTranscript);
+
+    expect(outputTranscript).toEqual({
+      confidence: 1,
+      words: [
+        {
+          word: ' ',
+          startTime: 0,
+          duration: 0.5,
+          outputStartTime: 0,
+          deleted: false,
+          key: '0',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'abc',
+          duration: 1,
+          startTime: 0.5,
+          outputStartTime: 0.5,
+          key: '1',
+          deleted: true,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 1.5,
+          duration: 0.5,
+          outputStartTime: 0.5,
+          deleted: false,
+          key: '2',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'def',
+          duration: 1,
+          startTime: 2,
+          outputStartTime: 1,
+          key: '3',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 3,
+          duration: 0.5,
+          outputStartTime: 2,
+          deleted: false,
+          key: '4',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+      ],
+    });
+  });
+});

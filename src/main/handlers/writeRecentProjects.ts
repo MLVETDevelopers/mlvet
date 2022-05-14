5e5cc229406e07bcddb06aee61a29d931cddb9e6
Patrick Brett <patrick@neara.com>
2022-05-14 19:36:02 +1000

    Fix broken preProcess test

--- src/main/handlers/writeRecentProjects.ts
+++ src/main/handlers/writeRecentProjects.ts
@@ -1,13 +1,13 @@
 import fs from 'fs/promises';
 import { RecentProject } from '../../sharedTypes';
-import { RECENT_PROJECTS_PATH } from '../util';
+import { getRecentProjectsPath } from '../util';
 
 type WriteRecentProjects = (recentProjects: RecentProject[]) => Promise<void>;
 
 const writeRecentProjects: WriteRecentProjects = async (recentProjects) => {
   const recentProjectsJson = JSON.stringify(recentProjects);
 
-  await fs.writeFile(RECENT_PROJECTS_PATH, recentProjectsJson);
+  await fs.writeFile(getRecentProjectsPath(), recentProjectsJson);
 };
 
 export default writeRecentProjects;

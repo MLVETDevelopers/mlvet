031da0ef5add8d9ad1675b2cab0a4f4770b32143
Vita1287 <jzho0079@student.monash.edu>
2022-05-14 18:11:53 +1000

    testing liveProcess with an edit

--- src/main/editDelete/__tests__/liveProcess.test.ts
+++ src/main/editDelete/__tests__/liveProcess.test.ts
@@ -107,4 +107,115 @@ describe('Test live-processing Transcription outputStartTime after edit/delete',
       ],
     });
   });
+
+  it('should produce expected Transcription after edit operation', () => {
+    const inputTranscript = {
+      confidence: 1,
+      words: [
+        {
+          word: ' ',
+          startTime: 0,
+          duration: 0.5,
+          outputStartTime: 0,
+          deleted: false,
+          key: '0',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'abc',
+          duration: 1,
+          startTime: 0.5,
+          outputStartTime: 0.5,
+          key: '1',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 1.5,
+          duration: 0.5,
+          outputStartTime: 1.5,
+          deleted: false,
+          key: '2',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'def',
+          duration: 1,
+          startTime: 2,
+          outputStartTime: 2,
+          key: '3',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 3,
+          duration: 0.5,
+          outputStartTime: 3,
+          deleted: false,
+          key: '4',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+      ],
+    };
+
+    const [, movedWord1] = inputTranscript.words;
+    const [movedWord0] = inputTranscript.words;
+    inputTranscript.words[1] = movedWord0;
+    inputTranscript.words[0] = movedWord1;
+    const outputTranscript = liveProcessTranscript(inputTranscript);
+
+    expect(outputTranscript).toEqual({
+      confidence: 1,
+      words: [
+        {
+          word: 'abc',
+          duration: 1,
+          startTime: 0.5,
+          outputStartTime: 0,
+          key: '1',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 0,
+          duration: 0.5,
+          outputStartTime: 1,
+          deleted: false,
+          key: '0',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+
+        {
+          word: ' ',
+          startTime: 1.5,
+          duration: 0.5,
+          outputStartTime: 1.5,
+          deleted: false,
+          key: '2',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: 'def',
+          duration: 1,
+          startTime: 2,
+          outputStartTime: 2,
+          key: '3',
+          deleted: false,
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+        {
+          word: ' ',
+          startTime: 3,
+          duration: 0.5,
+          outputStartTime: 3,
+          deleted: false,
+          key: '4',
+          fileName: 'PLACEHOLDER FILENAME',
+        },
+      ],
+    });
+  });
 });

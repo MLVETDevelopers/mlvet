2049361b31be5e5b7d512eeebbf0684d9c91be30
Patrick Brett <hello@pat.vc>
2022-05-19 15:40:57 +1000

    Close app when window closes

--- src/main/handlers/closeWindow.ts
+++ src/main/handlers/closeWindow.ts
@@ -1,3 +1,4 @@
+import { app } from 'electron';
 import { IpcContext } from '../types';
 
 type CloseWindow = (ipcContext: IpcContext) => void;
@@ -6,6 +7,9 @@ const closeWindow: CloseWindow = (ipcContext) => {
   const { mainWindow } = ipcContext;
 
   mainWindow.close();
+
+  // Since we only allow one window open for now, close the whole app when we shut the window
+  app.exit();
 };
 
 export default closeWindow;

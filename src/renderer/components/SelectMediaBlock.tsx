79098ad94cdd1a78127ebd16d35d4527b5e67536
Irmy van der Spuy <ivan0002@student.monash.edu>
2022-05-19 10:19:44 +1000

    UI response on drag of file import

--- src/renderer/components/SelectMediaBlock.tsx
+++ src/renderer/components/SelectMediaBlock.tsx
@@ -13,6 +13,12 @@ const SelectMediaBox = styled(Box)`
   }
 `;
 
+const SelectMediaBoxOverlay = styled(Box)`
+  width: 100%;
+
+  background: ${colors.grey[600]};
+`;
+
 const InnerBox = styled(Box)`
   border-style: dashed;
   border-width: 1px;
@@ -51,33 +57,40 @@ const SelectMediaBlock = ({
     setMediaFileName(fileName);
   };
 
-  // const [isDragOver, setDragOver] = useState<boolean>(false);
+  const [isDragOver, setDragOver] = useState<boolean>(false);
 
-  // // const exitDragOver = () => setDragOver(false);
-  // // const enterDragOver = () => setDragOver(true);
+  const exitDragOver = () => setDragOver(false);
+  const enterDragOver = () => setDragOver(true);
 
   const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
     e.stopPropagation();
+    e.preventDefault();
+    enterDragOver();
   };
 
   const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
     e.stopPropagation();
+    e.preventDefault();
+    exitDragOver();
   };
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
     e.stopPropagation();
+    e.preventDefault();
+    enterDragOver();
   };
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
     e.stopPropagation();
+    e.preventDefault();
+
+    exitDragOver();
 
     const draggedFiles = e.dataTransfer.files;
+
     if (draggedFiles.length > 0) {
-      /* Currently only supporting single file import */
+      /* Currently only supporting single file import - taking first file */
       const fileName = draggedFiles[0].name;
       const filePath = draggedFiles[0].path;
+
       setMediaFilePath(filePath);
       setMediaFileName(fileName);
       setIsAwaitingMedia(false);
@@ -86,25 +99,42 @@ const SelectMediaBlock = ({
 
   return (
     <SelectMediaBox
-      onDrop={handleDrop}
       onDragOver={handleDragOver}
       onDragEnter={handleDragEnter}
       onDragLeave={handleDragLeave}
     >
-      <InnerBox>
-        <Stack
-          direction="column"
-          alignItems="center"
-          justifyContent="space-between"
-          sx={{ height: '150px' }}
-        >
-          <Typography variant="p-300">Drag and drop file here</Typography>
-          <Typography variant="p-300">or</Typography>
-          <Button color="primary" onClick={selectMedia}>
-            Browse
-          </Button>
-        </Stack>
-      </InnerBox>
+      {isDragOver && (
+        <SelectMediaBoxOverlay onDrop={handleDrop}>
+          <InnerBox>
+            <Stack
+              direction="column"
+              alignItems="center"
+              justifyContent="space-between"
+              sx={{ height: '150px' }}
+            >
+              <Typography variant="p-300" padding="15%" fontSize="35px">
+                Drop here
+              </Typography>
+            </Stack>
+          </InnerBox>
+        </SelectMediaBoxOverlay>
+      )}
+      {isDragOver === false && (
+        <InnerBox>
+          <Stack
+            direction="column"
+            alignItems="center"
+            justifyContent="space-between"
+            sx={{ height: '150px' }}
+          >
+            <Typography variant="p-300">Drag and drop file here</Typography>
+            <Typography variant="p-300">or</Typography>
+            <Button color="primary" onClick={selectMedia}>
+              Browse
+            </Button>
+          </Stack>
+        </InnerBox>
+      )}
     </SelectMediaBox>
   );
 };

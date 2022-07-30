f41d5f3c0e77a8405669779608bfa12f322a2069
Chloe Brett <cbre0013@student.monash.edu>
2022-07-30 18:16:57 +1000

    Support dummy transcript

---
+++ src/main/config.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
+// Whether to use the sample transcription instead of actually transcribing.
+// Useful for testing.
+export const USE_DUMMY = false;

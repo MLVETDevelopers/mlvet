105ef3018872aeb9dda505459953fd3716933fd3
Chloe Brett <cbre0013@student.monash.edu>
2022-08-03 23:53:00 +1000

    remove empty test file

--- src/renderer/store/selection/__tests__/reducer.test.tsx
+++
@@ -1,6 +0,0 @@
-// import { SELECTION_RANGE_ADDED, SELECTION_RANGE_REMOVED, SELECTION_CLEARED } from '../actions';
-// import selectionReducer from '../reducer';
-
-describe('Selection reducer', () => {
-  // TODO(chloe) add tests
-});

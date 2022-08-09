7635a686cc1090f8be026fb8cc20b59fec9bc8d6
Justin Heaton <jhea0008@student.monash.edu>
2022-08-09 19:21:52 +1000

    Fix speechrecognizer with correct code

--- src/transcriptionAlternativeSpikes/NSSpeechRecognizer-apple/SpeechRecognizer/main.swift
+++ src/transcriptionAlternativeSpikes/NSSpeechRecognizer-apple/SpeechRecognizer/main.swift
@@ -1,43 +1,49 @@
-//
-//  main.swift
-//  SpeechRecognizer
-//
-//  Created by Justin on 28/7/2022.
-//
-
 import Foundation
 import Speech
+import AppKit
+//Referenced from https://github.com/dtinth/transcribe
+let app = NSApplication.shared
 
-print("Hello, World!")
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private let audioEngine = AVAudioEngine()
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
 
-SFSpeechRecognizer.requestAuthorization { (status) in
-    switch status {
-    case .notDetermined: print("Not determined")
-  case .restricted: print("Restricted")
-  case .denied: print("Denied")
-  case .authorized: print("We can recognize speech now.")
-  @unknown default: print("Unknown case")
-  }
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let inputNode = audioEngine.inputNode
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        SFSpeechRecognizer.requestAuthorization({ (authStatus: SFSpeechRecognizerAuthorizationStatus) in
+            self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            guard let speechRecognizer = self.speechRecognizer else { fatalError("Unable to create a SpeechRecognizer object") }
+            guard let recognitionRequest = self.recognitionRequest else { fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object") }
+            recognitionRequest.shouldReportPartialResults = true
+            let fileUrl = URL(fileURLWithPath: "/Users/justin/Desktop/hardBoiledEggs.mp4")
+            let request = SFSpeechURLRecognitionRequest(url: fileUrl)
+            speechRecognizer.recognitionTask(
+              with: request,
+              resultHandler: { (result, error) in
+                if let error = error {
+                    print("error: ")
+                    print(error)
+                } else if let result = result {
+                    if result.isFinal {
+                        print("\n Final Result: \n")
+                        print(result.bestTranscription.formattedString)
+                        exit(0)
+                    }
+                  print(result.bestTranscription.formattedString)
+                }
+            }
+        )
+        })
+    }
 }
-if let speechRecognizer = SFSpeechRecognizer() {
-  if speechRecognizer.isAvailable {
-      print("Recognizer")
-    // Use the speech recognizer
-      let fileUrl = URL(fileURLWithPath: "/Users/justin/Library/Mobile Documents/com~apple~CloudDocs/Test Files/sandwich_video.mov")
-      let request = SFSpeechURLRecognitionRequest(url: fileUrl)
-      speechRecognizer.supportsOnDeviceRecognition = true
-      speechRecognizer.recognitionTask(
-        with: request,
-        resultHandler: { (result, error) in
-          if let error = error {
-              print("error")
-          } else if let result = result {
-            print(result.bestTranscription.formattedString)
-          }
-      })
-      sleep(20)
-  }
-}
 
-
+let delegate = AppDelegate()
+app.delegate = delegate
+app.run()
 

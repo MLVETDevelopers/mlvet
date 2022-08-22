829147f8bf794b82f1eb8c9a7ad24ab9d5c0ac2c
Justin Heaton <jhea0008@student.monash.edu>
2022-08-22 17:21:47 +1000

    PR fixes

--- src/main/handlers/media/transcribe.ts
+++ src/main/handlers/media/transcribe.ts
@@ -4,21 +4,15 @@ import { TranscriptionEngine, TranscriptionFunction } from './transcribeTypes';
 import deepspeechTranscribeFunction from './transcriptionEngines/deepspeechTranscribeFunction';
 import dummyTranscribeFunction from './transcriptionEngines/dummyTranscribeFunction';
 
-const getTranscriptionFunction = new Map<
+const getTranscriptionFunction: Record<
   TranscriptionEngine,
   TranscriptionFunction
->();
-
-// Add the enum - function mapping for a transcription engine here
-getTranscriptionFunction.set(TranscriptionEngine.VOSK, voskTranscribeFunction);
-getTranscriptionFunction.set(
-  TranscriptionEngine.DEEPSPEECH,
-  deepspeechTranscribeFunction
-);
-getTranscriptionFunction.set(
-  TranscriptionEngine.DUMMY,
-  dummyTranscribeFunction
-);
+> = {
+  // Add the enum - function mapping for a transcription engine here
+  [TranscriptionEngine.VOSK]: voskTranscribeFunction,
+  [TranscriptionEngine.DUMMY]: dummyTranscribeFunction,
+  [TranscriptionEngine.DEEPSPEECH]: deepspeechTranscribeFunction,
+};
 
 /**
  * Runs the transcription using the selected transcription engine
@@ -32,8 +26,7 @@ const transcribe = async (
   transcriptionEngine: TranscriptionEngine
 ) => {
   const transcriptionFunction =
-    getTranscriptionFunction.get(transcriptionEngine) ??
-    dummyTranscribeFunction;
+    getTranscriptionFunction[transcriptionEngine] ?? dummyTranscribeFunction;
   return transcriptionFunction(project);
 };
 
--- src/main/handlers/media/transcriptionEngines/dummyTranscribeFunction.ts
+++ src/main/handlers/media/transcriptionEngines/dummyTranscribeFunction.ts
@@ -1,26 +1,7 @@
 import path from 'path';
 import fs from 'fs';
-import { MapCallback, PartialWord } from 'sharedTypes';
 import { TranscriptionFunction } from '../transcribeTypes';
-
-/**
- * Replace the start_time attribute with startTime (can be generalised further but shouldn't
- * need this once python outputs camelcase anyway)
- * @param word snake cased partial word
- * @returns camel cased partial word
- *
- */
-const camelCase: MapCallback<SnakeCaseWord, PartialWord> = (word) => ({
-  word: word.word,
-  duration: word.duration,
-  startTime: word.start_time,
-});
-
-interface SnakeCaseWord {
-  word: string;
-  duration: number;
-  start_time: number; // TODO: change this to camel case before it touches TS
-}
+import camelCase from './shared/transcribeFunctionSharedUtils';
 
 /**
  * Function to generate a dummy transcription by reading a hardcoded transcription file.

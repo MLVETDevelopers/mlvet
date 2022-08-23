96376e1c1ef71c9ee52860e2edf7496cc0b6a9ba
Emma Painter <epainter7@gmail.com>
2022-08-23 11:26:05 +1000

    Restore popover component

---
+++ src/renderer/components/RestorePopup.tsx
@@ -0,0 +1,78 @@
+import { Box, Popover, Typography } from '@mui/material';
+import { useState } from 'react';
+import colors from '../colors';
+
+interface RestorePopoverProps {
+  text: string;
+}
+
+const RestorePopover = ({ text }: RestorePopoverProps) => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <Popover
+      id="restore-popover"
+      open={open}
+      anchorEl={anchorEl}
+      onClose={handlePopoverClose}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      PaperProps={{
+        style: {
+          backgroundColor: 'transparent',
+          boxShadow: 'none',
+          borderRadius: 0,
+        },
+      }}
+    >
+      <Typography
+        sx={{
+          p: 2,
+          backgroundColor: colors.grey[600],
+          border: 1,
+          borderColor: colors.yellow[500],
+        }}
+      >
+        {text}
+      </Typography>
+      <Box
+        sx={{
+          position: 'relative',
+          mt: '10px',
+          '&::before': {
+            backgroundColor: colors.grey[600],
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            width: 9.5,
+            height: 9.5,
+            top: -15,
+            transform: 'rotate(45deg)',
+            left: 'calc(50% - 6px)',
+            borderBottom: 1,
+            borderRight: 1,
+            borderColor: colors.yellow[500],
+          },
+        }}
+      />
+    </Popover>
+  );
+};
+
+export default RestorePopover;

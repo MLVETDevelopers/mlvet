190c839d2c797e92c50b86f118a1a3fe1a582521
Hugh Henry <hhen0005@student.monash.edu>
2022-08-24 16:04:33 +1000

    Move vosk model into assets folder so that it gets packaged with app

--- src/main/handlers/media/transcriptionEngines/voskTranscribeFunction.ts
+++ src/main/handlers/media/transcriptionEngines/voskTranscribeFunction.ts
@@ -1,5 +1,6 @@
 import { MapCallback, PartialWord } from 'sharedTypes';
 import path from 'path';
+import fs from 'fs';
 import { getAudioExtractPath } from '../../../util';
 import { TranscriptionFunction } from '../transcribeTypes';
 import getVoskTranscript from '../../misc/getVoskTranscript';
@@ -19,8 +20,15 @@ const voskTranscribeFunction: TranscriptionFunction = async (project) => {
   const modelName = 'vosk-model-en-us-0.22';
   const modelPath: string = path.join(
     __dirname,
-    `../../../../voskModel/${modelName}`
+    '../../../../../.',
+    `assets/voskModel/${modelName}`
   );
+
+  if (!fs.existsSync(modelPath)) {
+    throw new Error(
+      `Model ${modelPath} not found. Have you downloaded the model and put it in the assets folder?`
+    );
+  }
 
   const filePath: string = getAudioExtractPath(project.id);
   const jsonTranscript = JSON.parse(

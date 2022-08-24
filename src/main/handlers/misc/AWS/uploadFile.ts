7f42ab7f7f4573fad003af3cef8d5d74b6cfb508
andyzhong8@gmail.com <andyzhong8@gmail.com>
2022-08-25 01:46:11 +1000

    Update uploadFile to work with getAudioExtract

--- src/main/handlers/misc/AWS/uploadFile.ts
+++ src/main/handlers/misc/AWS/uploadFile.ts
@@ -12,27 +12,30 @@ const s3Client = new S3Client({
   },
 });
 
-const file = './Me at the zoo.mp3'; // Path to and name of object. For example '../myFiles/index.js'.
-const fileStream = fs.createReadStream(file);
-
-// Set the parameters
-export const uploadParams = {
-  Bucket: 'fit3170',
-  // Add the required 'Key' parameter using the 'path' module.
-  Key: path.basename(file),
-  // Add the required 'Body' parameter
-  Body: fileStream,
-};
-
 // Upload file to specified bucket.
 // eslint-disable-next-line consistent-return
-export const run = async () => {
+const run = async (file: string) => {
+  const fileStream = fs.createReadStream(file);
+  // Set the parameters
+  const uploadParams = {
+    Bucket: 'fit3170',
+    // Add the required 'Key' parameter using the 'path' module.
+    Key: path.basename(file),
+    // Add the required 'Body' parameter
+    Body: fileStream,
+  };
   try {
     const data = await s3Client.send(new PutObjectCommand(uploadParams));
-    console.log('Success', data);
     return data; // For unit tests.
   } catch (err) {
-    console.log('Error', err);
+    if (typeof err === 'string') {
+      throw new Error(`Could not upload file to S3: ${err.toUpperCase}`);
+    } else if (err instanceof Error) {
+      throw new Error(`Could not upload file to S3: ${err.message}`);
+    } else {
+      return null;
+    }
   }
 };
-run();
+
+export default run;

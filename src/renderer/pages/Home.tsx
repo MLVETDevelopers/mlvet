09d837a17fe7fd81a47e019448420080db3ee724
Aretle <dfel0001@student.monash.edu>
2022-08-26 09:20:13 +1000

    Add transcription testing button

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -2,14 +2,20 @@ import { useSelector } from 'react-redux';
 import NewProjectBlock from '../components/ProjectCreation/NewProjectBlock';
 import RecentProjectsBlock from '../components/RecentProjectsBlock';
 import { ApplicationStore } from '../store/sharedHelpers';
+import googleTranscribeFunction from '../../main/handlers/helpers/transcriptionEngines/googleTranscribeFunction';
 
 const HomePage = () => {
   const hasRecentProjects = useSelector(
     (store: ApplicationStore) => store.recentProjects.length > 0
   );
 
+  const testTranscript = () => {
+    googleTranscribeFunction('project');
+  };
+
   return (
     <>
+      <button onClick={testTranscript}>test transcript</button>
       <NewProjectBlock isFullSize={!hasRecentProjects} />
       {hasRecentProjects && <RecentProjectsBlock />}
     </>

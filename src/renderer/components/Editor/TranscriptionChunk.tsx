49ab7fc3a6db8e8cce7141da2b824fd5d0ce43b1
Hugh Henry <hhen0005@student.monash.edu>
2022-08-27 11:04:06 +1000

    Update types for transcription chunk

--- src/renderer/components/Editor/TranscriptionChunk.tsx
+++ src/renderer/components/Editor/TranscriptionChunk.tsx
@@ -1,5 +1,8 @@
+import { MousePosition } from '@react-hook/mouse-position';
+import { Dispatch, SetStateAction } from 'react';
 import { TakeGroup, Word } from 'sharedTypes';
 import TakeGroupComponent from './TakeGroupComponent';
+import { DragState, WordMouseHandler } from './WordDragManager';
 import WordOuterComponent from './WordOuterComponent';
 
 function instanceofTakeGroup(object: any): object is TakeGroup {
@@ -8,13 +11,53 @@ function instanceofTakeGroup(object: any): object is TakeGroup {
 
 interface TranscriptionChunkProps {
   chunk: Word | TakeGroup;
+  chunkIndex: number;
+  onWordMouseDown: WordMouseHandler;
+  onWordMouseMove: WordMouseHandler;
+  dragState: DragState;
+  isWordBeingDragged: boolean;
+  mousePosition: MousePosition;
+  mouseThrottled: MousePosition;
+  dropBeforeIndex: number | null;
+  setDropBeforeIndex: Dispatch<SetStateAction<number | null>>;
+  cancelDrag: () => void;
+  editWord: any;
+  nowPlayingWordIndex: number | null;
 }
 
-const TranscriptionChunk = ({ chunk }: TranscriptionChunkProps) => {
+const TranscriptionChunk = ({
+  chunk,
+  chunkIndex,
+  onWordMouseDown,
+  onWordMouseMove,
+  dragState,
+  isWordBeingDragged,
+  mousePosition,
+  mouseThrottled,
+  dropBeforeIndex,
+  setDropBeforeIndex,
+  cancelDrag,
+  editWord,
+  nowPlayingWordIndex,
+}: TranscriptionChunkProps) => {
   return (
     <>
       {instanceofTakeGroup(chunk) ? (
-        <TakeGroupComponent takeGroup={chunk as TakeGroup} />
+        <TakeGroupComponent
+          takeGroup={chunk as TakeGroup}
+          chunkIndex={chunkIndex}
+          onWordMouseDown={onWordMouseDown}
+          onWordMouseMove={onWordMouseMove}
+          dragState={dragState}
+          isWordBeingDragged={isWordBeingDragged}
+          mousePosition={mousePosition}
+          mouseThrottled={mouseThrottled}
+          dropBeforeIndex={dropBeforeIndex}
+          setDropBeforeIndex={setDropBeforeIndex}
+          cancelDrag={cancelDrag}
+          editWord={editWord}
+          nowPlayingWordIndex={nowPlayingWordIndex}
+        />
       ) : (
         <WordOuterComponent />
       )}

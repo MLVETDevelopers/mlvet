cfbd30cf929da356bf7b4c4f73d3543c9d49856d
Hugh Henry <hhen0005@student.monash.edu>
2022-08-28 19:51:29 +1000

    Add fix for ffmpeg and ffprobe

--- src/main/ffUtils.ts
+++ src/main/ffUtils.ts
@@ -2,11 +2,11 @@ import ffmpegStaticPath from 'ffmpeg-static';
 import ffprobeStatic from 'ffprobe-static';
 
 export const ffmpegPath = ffmpegStaticPath.replace(
-  'app.asar',
-  'app.asar.unpacked'
+  'app.asar\\dist\\main\\',
+  'ffmpeg-static\\'
 );
 
 export const ffprobePath = ffprobeStatic.path.replace(
-  'app.asar',
-  'app.asar.unpacked'
+  'app.asar\\dist\\main\\',
+  'ffprobe-static\\'
 );

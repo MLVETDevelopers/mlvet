e90a024a3d82b680e17337018f76e671ee4a2e61
Chloe Brett <cbre0013@student.monash.edu>
2022-08-28 19:38:56 +1000

    Integrate take detection into pre process logic

--- src/main/takeDetection/sentenceSimilarity.ts
+++ src/main/takeDetection/sentenceSimilarity.ts
@@ -2,6 +2,7 @@ export default function getSimilarityScore(
   sentenceOne: string,
   sentenceTwo: string
 ): number {
-  sentenceOne.concat(sentenceTwo);
-  return 0.5;
+  // For mock, just return 1 (100% similar) if the sentences start with the same letter, otherwise 0 (0% similar) - allows for some meaningful testing
+
+  return sentenceOne[0].toLowerCase() === sentenceTwo[0].toLowerCase() ? 1 : 0;
 }

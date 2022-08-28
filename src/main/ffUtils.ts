66507ce16a7b0b2fe206c86d8d73c4901eb37c0c
Hugh Henry <hhen0005@student.monash.edu>
2022-08-28 17:34:38 +1000

    Add ffmpeg ffprobe packaging fixes

--- src/main/ffUtils.ts
+++ src/main/ffUtils.ts
@@ -1,21 +1,12 @@
 import ffmpegStaticPath from 'ffmpeg-static';
 import ffprobeStatic from 'ffprobe-static';
 
-console.log = require('electron-log').info;
-
-console.log('ffmpegstaticPath', ffmpegStaticPath);
-console.log('ffprobestaticPath', ffprobeStatic);
-
 export const ffmpegPath = ffmpegStaticPath.replace(
-  'app.asar',
-  'app.asar.unpacked'
+  'app.asar\\dist\\main\\',
+  'ffmpeg-static\\'
 );
-
-console.log(ffmpegPath);
 
 export const ffprobePath = ffprobeStatic.path.replace(
-  'app.asar',
-  'app.asar.unpacked'
+  'app.asar\\dist\\main\\',
+  'ffprobe-static\\'
 );
-
-console.log(ffprobePath);

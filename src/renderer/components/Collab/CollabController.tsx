19e8d8858957f2a8a453f74fc08fa7ed3a93c2e1
Chloe Brett <cbre0013@student.monash.edu>
2022-09-03 14:38:27 +1000

    render collab redux info

--- src/renderer/components/Collab/CollabController.tsx
+++ src/renderer/components/Collab/CollabController.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@mui/material';
 import { useState, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { ApplicationStore } from 'renderer/store/sharedHelpers';
 import CollabClient from '../../collabClient/CollabClient';
 
 const CollabController = () => {
+  const collab = useSelector((store: ApplicationStore) => store.collab);
+
   const [collabClient, setCollabClient] = useState<CollabClient | null>(null);
 
   const startCollabSession = useCallback(() => {
@@ -13,7 +17,24 @@ const CollabController = () => {
     setCollabClient(new CollabClient());
   }, [collabClient, setCollabClient]);
 
-  return <Button onClick={startCollabSession}>Start Collab Session</Button>;
+  if (collab === null) {
+    return <Button onClick={startCollabSession}>Start Collab Session</Button>;
+  }
+
+  return (
+    <div>
+      <div>Collab session code: {collab.sessionCode}</div>
+      <div>{collab.isHost ? "You're the host" : 'Editing as a guest'}</div>
+      <div>
+        Other collaborators connected:
+        {collab.clients
+          .filter((client) => client.id !== collab.ownClientId)
+          .map((client) => (
+            <div>{client.name}</div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CollabController;

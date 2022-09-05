12e9ddbd7766b58715d27f1981eb4e1081c2636a
Chloe Brett <cbre0013@student.monash.edu>
2022-09-05 16:37:03 +1000

    update server url, add typescript as a dependency

--- src/renderer/collabClient/config.ts
+++ src/renderer/collabClient/config.ts
@@ -1,7 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 
 export const LOG_VERBOSE = true;
-export const COLLAB_HOST = 'ws://localhost:5151';
+
+const USE_REMOTE_COLLAB = true;
+export const COLLAB_HOST = USE_REMOTE_COLLAB
+  ? 'ws://ec2-3-26-52-83.ap-southeast-2.compute.amazonaws.com:5151'
+  : 'ws://localhost:5151';
 
 // TODO: fix
 export const CLIENT_TO_SERVER_DELAY_SIMULATED = 0; // isTestEnv ? 0 : 0.5; // seconds

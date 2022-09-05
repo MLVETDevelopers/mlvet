e8b5eef514c5be0a4402597c9c827ef7a936235d
Chloe Brett <cbre0013@student.monash.edu>
2022-09-05 16:40:36 +1000

    no delay on server

--- src/collabServer/config.ts
+++ src/collabServer/config.ts
@@ -1,5 +1,7 @@
 const isTestEnv = process.env.JEST_WORKER_ID !== undefined;
+const isServerEnv = process.env.PROD_SERVER !== undefined;
 
-export const SERVER_TO_CLIENT_DELAY_SIMULATED = isTestEnv ? 0 : 0.5; // seconds
+export const SERVER_TO_CLIENT_DELAY_SIMULATED =
+  isTestEnv || isServerEnv ? 0 : 0.5; // seconds
 export const COLLAB_SERVER_PORT = 5151;
 export const LOG_VERBOSE = true;

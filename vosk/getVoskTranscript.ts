9de1cc2ae3e862af12691050847153728c181a3f
Hugh Henry <hhen0005@student.monash.edu>
2022-09-07 13:22:00 +1000

    Cleanup

--- vosk/getVoskTranscript.ts
+++ vosk/getVoskTranscript.ts
@@ -1,54 +1,62 @@
-import { Readable } from 'stream';
 import fs from 'fs';
+import { WaveFile } from 'wavefile';
+import vosk from 'vosk';
 
-const vosk = require('vosk');
-const wav = require('wav');
+interface WaveFormat {
+  audioFormat: number;
+  numChannels: number;
+  sampleRate: number;
+  byteRate: number;
+  blockAlign: number;
+  bitsPerSample: number;
+  extraParams: number[];
+}
 
-const getVoskTranscript = (modelPath: string, filePath: string) => {
+interface WaveData {
+  chunkId: string;
+  chunkSize: number;
+  samples: number[];
+}
+
+const getVoskTranscript = async (modelPath: string, filePath: string) => {
   vosk.setLogLevel(-1);
   const model = new vosk.Model(modelPath);
-  const wfReader = new wav.Reader();
-  const wfReadable = new Readable().wrap(wfReader);
 
-  const jsonTranscript: Promise<string> = new Promise((resolve) => {
-    wfReader.on('format', async (res: any) => {
-      if (res.audioFormat !== 1 || res.channels !== 1) {
-        console.error('Audio file must be WAV format mono PCM.');
-        process.exit(1);
-      }
+  const wav = new WaveFile();
+  wav.fromBuffer(fs.readFileSync(filePath));
 
-      const { sampleRate } = res;
-      const rec = new vosk.Recognizer({ model, sampleRate });
-      rec.setMaxAlternatives(1); // reduced from 10
-      rec.setWords(true);
-      rec.setPartialWords(true); // what is this idek
-      // eslint-disable-next-line no-restricted-syntax
-      for await (const data of wfReadable) {
-        rec.acceptWaveform(data);
-      }
-      const transcript: string = JSON.stringify(rec.finalResult(rec), null, 4);
-      console.log('=================================================');
-      console.log(transcript);
-      console.log('=================================================');
-      rec.free();
-      resolve(transcript);
-    });
-  });
+  const wavFormat = wav.fmt as WaveFormat;
+  if (wavFormat.audioFormat !== 1 || wavFormat.numChannels !== 1) {
+    console.error('Audio file must be WAV format mono PCM.');
+    process.exit(1);
+  }
 
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+  console.log('=========================================================');
+  console.log(modelPath);
+  console.log(filePath);
+  console.log(model);
+  console.log('=========================================================');
 
-  fs.createReadStream(filePath, { highWaterMark: 4096 })
-    .pipe(wfReader)
-    .on('finish', () => {
-      model.free();
-    });
+  const rec = new vosk.Recognizer({ model, sampleRate: 16000 });
+  rec.setMaxAlternatives(1); // reduced from 10
+  rec.setWords(true);
+  rec.setPartialWords(true); // what is this idek
 
-  return jsonTranscript;
+  console.log('===================================================');
+  console.log('got here');
+
+  const wavData = wav.data as WaveData;
+
+  console.log(wavData);
+
+  rec.acceptWaveform(wavData.samples);
+
+  const transcript: string = JSON.stringify(rec.finalResult(rec), null, 4);
+
+  rec.free();
+  model.free();
+
+  return transcript;
 };
 
 export default getVoskTranscript;

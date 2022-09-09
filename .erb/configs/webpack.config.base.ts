a84f87ed75549e3a6ce1e08d353c95e12dfe6d38
Hugh Henry <hhen0005@student.monash.edu>
2022-09-09 20:52:25 +1000

    Add webpack back

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -7,7 +7,12 @@ import webpackPaths from './webpack.paths';
 import { dependencies as externals } from '../../release/app/package.json';
 
 const configuration: webpack.Configuration = {
-  externals: [...Object.keys(externals || {})],
+  externals: [
+    ...Object.keys(externals || {}),
+    nodeExternals({
+      allowlist: [/^(?!(^(ffi-napi)$)).*$/i],
+    }),
+  ],
 
   stats: 'errors-only',
 

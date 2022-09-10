d320f56e55e75f8debda1507189655320f2efd92
Hugh Henry <hhen0005@student.monash.edu>
2022-09-10 10:32:37 +1000

    Initial rewrite of vosk into typescript with koffi

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -7,12 +7,7 @@ import webpackPaths from './webpack.paths';
 import { dependencies as externals } from '../../release/app/package.json';
 
 const configuration: webpack.Configuration = {
-  externals: [
-    ...Object.keys(externals || {}),
-    nodeExternals({
-      allowlist: [/^(?!(^(ffi-napi)$)).*$/i],
-    }),
-  ],
+  externals: [...Object.keys(externals || {})],
 
   stats: 'errors-only',
 

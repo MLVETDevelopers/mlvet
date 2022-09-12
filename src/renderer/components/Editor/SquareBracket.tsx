e57397ed73af83b167072c8cf493a4fbed792554
Taz17 <mutaz.ashhab@gmail.com>
2022-09-13 09:45:12 +1000

    Add SquareBracketBackground to SquareBracket

--- src/renderer/components/Editor/SquareBracket.tsx
+++ src/renderer/components/Editor/SquareBracket.tsx
@@ -1,13 +1,28 @@
+import styled from '@emotion/styled';
 import { Box } from '@mui/system';
 
 interface Props {
   isLast: boolean;
 }
 
+const SquareBracketBackground = styled(Box)({
+  height: '100%',
+  width: '16px',
+  opacity: 0,
+
+  '&:hover': {
+    opacity: 0.5,
+    backgroundColor: '#FFB355',
+    borderRadius: '8px',
+    borderWidth: '10px',
+  },
+});
+
 const SquareBracket = ({ isLast }: Props) => {
   const bottomWidth = isLast ? '2px' : '0px';
   return (
     <Box
+      id="squareBracket"
       sx={{
         height: '60px',
         width: '15px',
@@ -18,7 +33,9 @@ const SquareBracket = ({ isLast }: Props) => {
         borderTopWidth: '2px',
         borderBottomWidth: bottomWidth,
       }}
-    />
+    >
+      <SquareBracketBackground id="squareBracketBackground" />
+    </Box>
   );
 };
 

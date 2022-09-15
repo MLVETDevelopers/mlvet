0e990580bb49b8a3f58bbf6c87a9fa94f06a2f5c
Justin Heaton <jhea0008@student.monash.edu>
2022-09-15 23:19:54 +1000

    PR comment fix

--- src/renderer/editor/editWord.ts
+++ src/renderer/editor/editWord.ts
@@ -10,6 +10,9 @@ const editWord = () => {
     return;
   }
   const ranges = getSelectionRanges();
+  if (ranges.length === 0) {
+    return;
+  }
   // Assume that only one word is selected, thus there is only one range and start/end indexes are the same
   const index = ranges[0].startIndex;
   const { word } = words[index];

1dc9d39c050b9a50b4c888a78e8aad393cdeba06
andyzhong8@gmail.com <andyzhong8@gmail.com>
2022-09-22 18:57:17 +1000

    update reducer to handle pause/play state

--- src/renderer/store/playback/reducer.ts
+++ src/renderer/store/playback/reducer.ts
@@ -1,17 +1,55 @@
 import { Reducer } from 'redux';
 import { Action } from '../action';
 import { ApplicationStore, initialStore } from '../sharedHelpers';
-import { VIDEO_PLAYING } from './actions';
+import { VIDEO_PLAYING, VIDEO_SEEK, VIDEO_SKIP } from './actions';
 
-const updatePlaybackPauseReducer: Reducer<
-  ApplicationStore['isVideoPlaying'],
-  Action<boolean>
-> = (isVideoPlaying = initialStore.isVideoPlaying, action) => {
+const playbackReducer: Reducer<ApplicationStore['playback'], Action<any>> = (
+  playback = initialStore.playback,
+  action
+) => {
   if (action.type === VIDEO_PLAYING) {
-    return action.payload as boolean;
+    const playState = action.payload as boolean;
+    return {
+      playbackPlaying: playState,
+      playbackTime: playback.playbackTime,
+      playbackLastUpdated: playback.playbackLastUpdated,
+    };
   }
 
-  return isVideoPlaying;
+  if (action.type === VIDEO_SEEK) {
+    const timeState = action.payload as number;
+    return {
+      playbackPlaying: playback.playbackPlaying,
+      playbackTime: timeState,
+      playbackLastUpdated: playback.playbackLastUpdated,
+    };
+  }
+
+  if (action.type === VIDEO_SEEK) {
+    const timeState = action.payload as number;
+    return {
+      playbackPlaying: playback.playbackPlaying,
+      playbackTime: timeState,
+      playbackLastUpdated: playback.playbackLastUpdated,
+    };
+  }
+
+  if (action.type === VIDEO_SKIP) {
+    const timeState = action.payload as number;
+    if (!playback.playbackPlaying) {
+      return {
+        playbackPlaying: playback.playbackPlaying,
+        playbackTime: playback.playbackTime + timeState,
+        playbackLastUpdated: playback.playbackLastUpdated,
+      };
+    }
+
+    if (playback.playbackPlaying) {
+      console.log('hello');
+    }
+  }
+
+  return playback;
 };
 
-export default updatePlaybackPauseReducer;
+export default playbackReducer;

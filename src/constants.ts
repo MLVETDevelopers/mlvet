e2b4be4128bc06e8956be31ca5be703de006e063
ltod0001 <ltod0001@student.monash.edu>
2022-09-22 21:42:54 +1000

    add comment to contants file

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export const CURRENT_SCHEMA_VERSION = 2;
 export const URL_USER_FEEDBACK_FORM = 'https://forms.gle/YNozsPZxF4kxSuud8';
 export const REPO_OWNER_USERNAME = 'chloebrett';
 export const REPO_NAME = 'mlvet';
+// replace 'null' with commented token to switch on issue creation from 'mlvet-bug-reporter' account
 export const REPO_AUTH_TOKEN = null; // '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';

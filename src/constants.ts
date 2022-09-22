b538a9f5c06d31540ebbb9131d52afe41cb66248
LiamTodd <toddliam26@gmail.com>
2022-09-22 22:09:51 +1000

    put back issue labelling after stupid realisation

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,9 @@ export const CURRENT_SCHEMA_VERSION = 2;
 export const URL_USER_FEEDBACK_FORM = 'https://forms.gle/YNozsPZxF4kxSuud8';
 export const REPO_OWNER_USERNAME = 'chloebrett';
 export const REPO_NAME = 'mlvet';
+export const BUG_REPORT_LABEL = 'user-reported bug';
+export const BUG_REPORT_LABEL_DESC =
+  'issues with this label originate from in-app user feedback';
+export const BUG_REPORT_LABEL_COLOUR = 'f59e42';
 // replace 'null' with commented token to switch on issue creation from 'mlvet-bug-reporter' account
-export const REPO_AUTH_TOKEN = null; // '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';
+export const REPO_AUTH_TOKEN = null; // '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';

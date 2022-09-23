7770c36ad4a0870832df214f6c538f28b2a1654e
LiamTodd <toddliam26@gmail.com>
2022-09-23 21:57:33 +1000

    refactor bug reporting with nextjs server

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const CURRENT_SCHEMA_VERSION = 2;
 export const URL_USER_FEEDBACK_FORM = 'https://forms.gle/YNozsPZxF4kxSuud8';
-export const REPO_OWNER_USERNAME = 'chloebrett';
-export const REPO_NAME = 'mlvet';
-export const BUG_REPORT_LABEL = 'user-reported bug';
-export const BUG_REPORT_LABEL_DESC =
-  'issues with this label originate from in-app user feedback';
-export const BUG_REPORT_LABEL_COLOUR = 'f59e42';
-// replace 'null' with commented token to switch on issue creation from 'mlvet-bug-reporter' account
-// Note: please do not remove 'atob'. It decrypts and encrypted version of the access token because GitHub doesnt like seeing its access tokens in its repos
-export const REPO_AUTH_TOKEN = null; // atob('Z2hwX0dZckFYS1NobnNJeURRb0poUElDem1RdHZBcmxtSzNla1dzZQ==');
--- src/main/handlers/misc/reportBug.ts
+++ src/main/handlers/misc/reportBug.ts
@@ -1,53 +1,21 @@
-import { Octokit } from '@octokit/rest';
-import {
-  REPO_OWNER_USERNAME,
-  REPO_NAME,
-  BUG_REPORT_LABEL,
-  BUG_REPORT_LABEL_DESC,
-  BUG_REPORT_LABEL_COLOUR,
-  REPO_AUTH_TOKEN,
-} from '../../../constants';
+import fetch from 'node-fetch';
 
 const reportBug = async (title: string, body: string) => {
-  const octokit = new Octokit({
-    auth: REPO_AUTH_TOKEN,
-  });
-
   try {
-    // get labels
-    const labelResponse = await octokit.request(
-      'GET /repos/{owner}/{repo}/labels',
+    await fetch(
+      'https://mlvet-bug-reporter-mlvet-bug-reporter-9xw6.vercel.app/api/reportBug',
       {
-        owner: REPO_OWNER_USERNAME,
-        repo: REPO_NAME,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          // eslint-disable-next-line object-shorthand
+          title: title,
+          description: body,
+        }),
       }
     );
-
-    // check if bug report label exists
-    const containsBugReportLabel = labelResponse.data
-      .map((label) => label.name)
-      .includes(BUG_REPORT_LABEL);
-
-    // if not, create it
-    if (!containsBugReportLabel) {
-      await octokit.request('POST /repos/{owner}/{repo}/labels', {
-        owner: REPO_OWNER_USERNAME,
-        repo: REPO_NAME,
-        name: BUG_REPORT_LABEL,
-        description: BUG_REPORT_LABEL_DESC,
-        color: BUG_REPORT_LABEL_COLOUR,
-      });
-    }
-
-    // create issue
-    await octokit.request('POST /repos/{owner}/{repo}/issues', {
-      owner: REPO_OWNER_USERNAME,
-      repo: REPO_NAME,
-      title,
-      body,
-      labels: [BUG_REPORT_LABEL],
-    });
-
     return 200;
   } catch (e) {
     console.error(e);

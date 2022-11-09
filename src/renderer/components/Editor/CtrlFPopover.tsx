e29b0c890348c6e6061530619170b51b535ae25a
Thien Nguyen <tngu0041@student.monash.edu>
2022-11-09 16:09:31 +1100

    Fixed Ctrl F Popover being rendered beneath TranscriptionBlock

--- src/renderer/components/Editor/CtrlFPopover.tsx
+++ src/renderer/components/Editor/CtrlFPopover.tsx
@@ -112,6 +112,7 @@ const SearchBoxPopover = ({ transcription }: SearchBoxPopoverProps) => {
         right: 50,
         width: 350,
         boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
+        zIndex: 1,
       }}
     >
       <Stack
